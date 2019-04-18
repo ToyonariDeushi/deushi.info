6e4cb38b20493783b0b0616a7b3dd7ee63ce5041
出牛 豊成 <zombieship@gmail.com>
2019-04-18 19:04:14 +0900

    gulpが走らない問題を解決

---
+++ dist/include/profileArea.php
@@ -0,0 +1,19 @@
+<div class="profileArea">
+    <div class="profileArea__photo"><img src="<?php echo $DOCUMENT_ROOT_URL; ?>assets/images/image-profile.jpg" alt=""></div>
+    <div class="profileArea__intro">
+        <dl class="profileDefinition">
+            <dt>名前</dt>
+            <dd><ruby>出牛<rt>でうし</rt></ruby> <ruby>豊成<rt>とよなり</rt></ruby></dd>
+            <dt>生年月日</dt>
+            <dd>1983年10月23日</dd>
+            <dt>趣味</dt>
+            <dd>サバゲー、アニメ、ゲーム、音楽</dd>
+        </dl>
+        <p>都内の某社にてWebディレクターをやっています。<br>制作上がりのWebディレクターというのが強みです。</p>
+        <ul class="profileSns">
+            <li class="icon-facebook"><a href="https://www.facebook.com/HYPER.USHI" target="_blank" class="newTab">Facebook</a></li>
+            <li class="icon-twitter"><a href="https://twitter.com/HYPER_USHI" target="_blank" class="newTab">Twitter</a></li>
+            <li class="icon-github"><a href="https://github.com/ToyonariDeushi" target="_blank" class="newTab">Github</a></li>
+        </ul>
+    </div>
+</div>
--- dist/include/sideContainer.php
+++ dist/include/sideContainer.php
@@ -8,45 +8,39 @@
     <div class="subContainer__category">
         <h2 class="subContainer__category-title">カテゴリー</h2>
         <ul>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">趣味（10）</a></li>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">育児（20）</a></li>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">仕事（30）</a></li>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">猫（40）</a></li>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">レビュー（50）</a></li>
-            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">未分類（60）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">趣味（0）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">育児（0）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">仕事（0）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">猫（0）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">レビュー（0）</a></li>
+            <li><a href="<?php echo $DOCUMENT_ROOT_URL; ?>">未分類（1）</a></li>
         </ul>
     </div>
     <div class="subContainer__recentPosts">
         <h2 class="subContainer__recentPosts-title">最近の記事</h2>
         <ul class="recentPosts-list">
-<?php
-for ($i = 0; $i < 6; $i++) {
-?>
             <li class="recentPosts-item">
-                <a href="<?php echo $DOCUMENT_ROOT_URL; ?>article.html">
+                <a href="<?php echo $DOCUMENT_ROOT_URL; ?>article/prologue.html">
                     <div class="recentPosts-wrap">
                         <div class="recentPosts-media">
-                            <img src="<?php echo $DOCUMENT_ROOT_URL; ?>assets/images/sample-thum.jpg" alt="">
+                            <img src="<?php echo $DOCUMENT_ROOT_URL; ?>assets/images/mv-prologue.jpg" alt="サムネイル">
                         </div>
                         <div class="recentPosts-content">
-                            <div class="recentPosts-title">それも今とうとうその永続ようというもののために行きですた。しかるに場合に意味帰りはよほどその解釈ででまでが行きがみるでからは相違立てるんないから、まだにも申し上げたなました。</div>
+                            <div class="recentPosts-title">過去にもブログサイトをやっていたことはありますが、忙しさゆえ、サイトを閉鎖してしまいました。あれから数年、ようやく重たい腰を上げ、新たにブログサイトを立ち上げることとなりました（祝）</div>
                             <div class="recentPosts-meta">
-                                <time datetime="2019-01-01" class="recentPosts-date">2019.01.01</time>
-                                <span class="recentPosts-category">趣味</span>
+                                <time datetime="2019-03-29" class="recentPosts-date">2019.03.29</time>
+                                <span class="recentPosts-category">未分類</span>
                             </div>
                         </div>
                     </div>
                 </a>
             </li>
-<?php
-}
-?>
         </ul>
     </div>
     <div class="subContainer__archivePosts">
         <h2 class="subContainer__archivePosts-title">アーカイブ</h2>
         <select class="subContainer__archivePosts-list">
-            <option value="" selected="selected">選択</option>
+            <option value="" selected="selected">選択▼</option>
             <option value="<?php echo $DOCUMENT_ROOT_URL; ?>">2019年01月</option>
             <option value="<?php echo $DOCUMENT_ROOT_URL; ?>">2018年12月</option>
             <option value="<?php echo $DOCUMENT_ROOT_URL; ?>">2018年11月</option>
